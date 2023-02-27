d10cc70d08f8641fe4ed8dbb148e49b4ed689d14
Arthur Detomi <arthurdetomi@gmail.com>
2023-02-27 19:14:13 -0300

    Criando primeiro componente

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,12 +1,13 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import './index.css';
+import Primeiro from './components/basicos/Primeiro';
 
 const tag = <strong>Olá React</strong>;
 
 ReactDOM.render(
     <div>
-        {tag}
+        <Primeiro></Primeiro>
     </div>, 
     document.getElementById('root')
 );
