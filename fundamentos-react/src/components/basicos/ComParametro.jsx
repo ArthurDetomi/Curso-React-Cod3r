224428feab4b6039a16659d6ddf9d7e8c444e7a3
Arthur Detomi <arthurdetomi@gmail.com>
2023-02-27 19:46:48 -0300

    Propriedades são somente leitura não é possível altera-las

--- fundamentos-react/src/components/basicos/ComParametro.jsx
+++ fundamentos-react/src/components/basicos/ComParametro.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 
+// Propriedades são somente leitura não é possível altera-las
+
 export default function ComParametro(props) {
     const status = props.nota >= 7 ? 'Aprovado' : 'Reprovado';
+    const notaInt = Math.ceil(props.nota);
     return ( 
         <div>
             <h2>{props.titulo}</h2>
             <p>
                 <strong>{props.aluno} </strong> 
                 tem nota 
-                <strong> {props.nota} </strong>
+                <strong> {notaInt} </strong>
                 e foi 
                 <strong> {status}</strong>
             </p>
