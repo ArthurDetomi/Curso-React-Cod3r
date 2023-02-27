efb0dc2af0d2cb9ba3883831de7d9931f0256f87
Arthur Detomi <arthurdetomi@gmail.com>
2023-02-27 19:21:08 -0300

    Utilizando JSX dentro do componente
    Para usar o JSX dentro do componente deve se fazer o import do React

--- fundamentos-react/src/components/basicos/Primeiro.js
+++ fundamentos-react/src/components/basicos/Primeiro.js
@@ -1,3 +1,14 @@
+import React from "react";
+
+// Para usar o JSX dentro do componente deve se fazer o import do React
 export default function Primeiro() {
-    return 'Primeiro Componente';
+    const msg = 'Seja bem vindo!';
+    return (
+        <div>
+            <h2>Primeiro Componente</h2>
+            <p>
+                {msg}
+            </p>
+        </div>
+    ); 
 }
